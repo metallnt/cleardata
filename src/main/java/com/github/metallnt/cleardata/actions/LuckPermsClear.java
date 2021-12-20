b4f667d2c33d7572269cc51e8f58f129fcac5d12
”Metall” <serzvasington@gmail.com>
2021-12-21 01:06:01 +0500

    4. В процессе по командам (...) 10. Изменен способ удаления данных Autorank (-)[full] 11. Изменен способ удаления данных LuckPerms (-)[full]

--- src/main/java/com/github/metallnt/cleardata/actions/LuckPermsClear.java
+++ src/main/java/com/github/metallnt/cleardata/actions/LuckPermsClear.java
@@ -1,7 +1,12 @@
 package com.github.metallnt.cleardata.actions;
 
 import com.github.metallnt.cleardata.ClearData;
+import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import java.util.Objects;
+import java.util.UUID;
 
 /**
  * Class com.github.metallnt.cleardata.actions
@@ -13,14 +18,24 @@ import org.bukkit.Bukkit;
 public class LuckPermsClear {
 
     private final ClearData plugin;
+    private LuckPerms api;
 
     public LuckPermsClear(ClearData plugin) {
         this.plugin = plugin;
     }
 
+    private void clearData(String player) {
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        if (provider != null) {
+            api = provider.getProvider();
+            UUID uuid = Objects.requireNonNull(api.getUserManager().getUser(player)).getUniqueId();
+            api.getUserManager().deletePlayerData(uuid);
+        }
+    }
+
     public void clear(String player) {
         if (plugin.getSettingsConfig().getLuckPerms()) {
-            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "lp user " + player + " clear");
+            clearData(player);
             plugin.getServer().getConsoleSender().sendMessage("Из плагина LuckPerms удалены данные игрока " + player);
         } else {
             plugin.getServer().getConsoleSender().sendMessage("Очистка данных LuckPerms отключена в конфиге");
